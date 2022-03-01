1e93292faba26e19f4ad9a0c786b409fdf1bef6a
Grace Thompson <Grace.thompson17@aol.com>
2022-02-28 20:39:03 -0600

    first commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let apple = 6;
+window.addEventListener('load',function(){
+let takeoff=document.getElementById('takeoff');
+let landing=document.getElementById('landing');
+let missionAbort=document.getElementById('missionAbort');
+let status = document.getElementById('flightStatus');
+let background =document.getElementById('shuttleBackground');
+let shuttleHeight = document.getElementById('spaceShuttleHeight');
+takeoff.addEventListener('click',function(){
+    let clickReady = window.confirm('Confirm that the shuttle is ready for takeoff');
+    if(clickReady){
+        status.innerHTML='Shuttle in flight';
+        background.style.backgroundColor = 'blue';
+        shuttleHeight.innerHTML =parseInt(shuttleHeight.innerHTML)+10000;
+    }
+});
+landing.addEventListener('click',function(){
+
+});
+missionAbort.addEventListener('click',function(){
+
+});
+});
